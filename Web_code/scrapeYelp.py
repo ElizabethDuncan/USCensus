597f53b1aeee7f68991f296776cbe5a80520f5ce
ElizabethDuncan <allison@allies-mbp.olin.edu>
2014-04-19 23:14:00 -0400

    change image of competitors

--- Web_code/scrapeYelp.py
+++ Web_code/scrapeYelp.py
@@ -21,15 +21,19 @@ def getAddresses(coor, location, descrip, mapDistance):
 	addressList = []
 	businnesCoorList = []
 	link = "http://www.yelp.com/search?find_desc=" + str(descrip) + "&find_loc=" + str(location) + "&ns=1&ls=ba0120fc3b21754c"
-
+	print link
 
 	r = requests.get(link)
 	text =  r.text
+	print 'requests.status_code:', r.status_code
+	print '<address> in text:', '<address>' in text
 
 	places = [m.start() for m in re.finditer('<address>', text)]
+	print places
 
 
 	for place in places:
+		print place
 		address = find_between(text[int(place)::], "address", "</address>" )
 		address = address.replace("<br>", " ");
 		addressList.append(address)
@@ -38,6 +42,7 @@ def getAddresses(coor, location, descrip, mapDistance):
 
 
 	for address in addressList:
+		print address
 		lat = coor[0]
 		lng = coor[1]
 		currentLatLong = getLatLong.getLatLong(address)
@@ -49,4 +54,9 @@ def getAddresses(coor, location, descrip, mapDistance):
 	return businnesCoorList
 
 
+
 #getAddresses((38.252665, -85.758456), "Louisville, KY", "coffee shop", 0.03)
+#getAddresses([40.7358633, -73.9910835],"Union Square, NY","yoga studio",0.0152083333333)
+
+
+
